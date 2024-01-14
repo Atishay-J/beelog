325934201500dbf9e9d0099a4964bfd57a126150
Atishay <hritikjain707@gmail.com>
2024-01-14 16:02:50 +0530

    feat: add data sanitization

--- server/src/routes/blog.route.ts
+++ server/src/routes/blog.route.ts
@@ -1,6 +1,12 @@
 import { Router } from 'express';
 import { createBlog } from '../controllers/blog.controller';
+import { body } from 'express-validator';
 
 export const blog = Router();
 
-blog.post('/create-blog', createBlog);
+blog.post(
+  '/create-blog',
+  body('title').notEmpty(),
+  body('content').notEmpty().escape(),
+  createBlog
+);
