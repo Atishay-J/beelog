68003848ef5584c4ab900c7bdfaf7f1582324902
Atishay <hritikjain707@gmail.com>
2024-01-14 10:50:01 +0530

    chore: add logo

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,5 +1,6 @@
-import { HStack } from '@chakra-ui/react';
+import { HStack, Image } from '@chakra-ui/react';
 import Buttons from '../Buttons';
+import { Link } from 'react-router-dom';
 
 interface NavbarProps {
   variant?: 'CREATE' | 'SAVE';
@@ -12,10 +13,21 @@ export default function Navbar({ variant = 'CREATE' }: NavbarProps) {
       w="100%"
       boxShadow="0px 0px 10px 2px rgba(230,230,230,0.4)"
       alignItems="center"
-      justifyContent="flex-end"
-      padding="0.5rem 1rem"
+      justifyContent="center"
+      padding="0.7rem 3rem"
     >
-      {variant === 'CREATE' ? <Buttons.CreateBlog /> : <Buttons.SaveBlog />}
+      <HStack
+        w="100%"
+        alignItems="center"
+        justifyContent="space-between"
+        maxW="1920px"
+      >
+        <Link to="/">
+          {' '}
+          <Image src="/assets/beelog.png" alt="Bee.log" w="6.5rem" h="auto" />
+        </Link>
+        {variant === 'CREATE' ? <Buttons.CreateBlog /> : <Buttons.SaveBlog />}
+      </HStack>
     </HStack>
   );
 }
