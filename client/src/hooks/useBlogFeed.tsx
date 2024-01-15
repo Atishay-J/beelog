e775b2321ffbfc65e1f636f1da3e2d095e4afe5b
Atishay <hritikjain707@gmail.com>
2024-01-15 15:46:46 +0530

    fix: multi feed fetch

--- client/src/hooks/useBlogFeed.tsx
+++ client/src/hooks/useBlogFeed.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { debounce } from '../utils';
 import { BlogResponseType } from '../types';
 import { fetchBlogs } from '../api';
@@ -7,7 +7,6 @@ export const useBlogFeed = () => {
   const [blogs, setBlogs] = useState<BlogResponseType[] | []>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [blogPageNumber, setBlogPageNumber] = useState(1);
-  const isFirstRender = useRef(true);
   const [hasMoreData, setHasMoreData] = useState(true);
 
   const updateBlogData = useCallback(async (pageNumber: number) => {
@@ -35,14 +34,7 @@ export const useBlogFeed = () => {
       window.removeEventListener('scroll', debouncedHandleScroll);
     }
     return () => window.removeEventListener('scroll', debouncedHandleScroll);
-  }, [isLoading, hasMoreData]);
-
-  useEffect(() => {
-    if (!isLoading && isFirstRender.current) {
-      updateBlogData(blogPageNumber);
-      isFirstRender.current = false;
-    }
-  }, [updateBlogData, blogPageNumber, isLoading]);
+  }, [hasMoreData]);
 
   useEffect(() => {
     if (blogPageNumber) {
